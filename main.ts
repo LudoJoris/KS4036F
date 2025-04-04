5bdc7c1a146b24dae23c294996dd88cd573e2b09
lujo <58606086+LudoJoris@users.noreply.github.com>
2025-04-04 19:15:15 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,212 +1,74 @@
-
-let address = 0x30
+let i2c_address = 48
 
 enum Motorlist {
-    //% block="links"
-    M1 = 1,
-    //% block="rechts"
-    M2 = 2
+  //% block="links"
+  M0 = 0,
+  //% block="rechts"
+  M1 = 1
 }
 
-enum Direction1 {
-    //% block="vooruit"
-    vooruit = 0,
-    //% block="achteruit"
-    achteruit = 1
+enum Direction {
+  //% block="vooruit"
+  D0 = 0,
+  //% block="achteruit"
+  D1 = 1
 }
 enum LED_rgb_L_R {
-    //% block="LED_R"
-    LED_R = 1,
-    //% block="LED_L"
-    LED_L = 0,
-}
-
-enum LED_color {
-    //% block="red"
-    red1 = 1,
-    //% block="green"
-    green1 = 2,
-    //% block="blue"
-    blue1 = 3,
-    //% block="cyan"
-    cyan = 4,
-    //% block="purple"
-    purple = 5,
-    //% block="white"
-    white = 6,
-    //% block="yellow"
-    yellow = 7,
-    //% block="Turn off LED"
-    black = 8,
-
-}
-enum pwm_led_l {
-    //% black="red"
-    pwm_red_r = 0x08,
-    //% black="green"
-    pwm_green_r = 0x07,
-    //% black="blue"
-    pwm_blue_r = 0x06,
-}
-
-enum pwm_led_r {
-    //% black="red"
-    pem_red_l = 0x09,
-    //% black="green"
-    pwm_green_l = 0x0a,
-    //% black="blue"
-    pwm_blue_l = 0x05,
+  //% block="links"
+  L0 = 0,
+  //% block="rechts"
+  L1 = 1
 }
 
 //% color="#AA278D"
-namespace MiniCar {
+namespace SmartCar {
 
-    //% block="motor = | %motor richting = | $direction snelheid = $pwmvalue"
-    //% pwmvalue.min=0 pwmvalue.max=255 
+    //% block="motor = | %motor richting = | $richting snelheid = $snelheid"
+    //% snelheid.min=0 snelheid.max=255 
     //% group="Motor" weight=65
-    export function motor(motor: Motorlist, direction: Direction1, pwmvalue: number) {
+    export function motor(motor: Motorlist, richting: Direction, snelheid: number) {
         switch (motor) {
-            case 1: // M1电机控制
-                if (direction) { 
-                    motor_i2cWrite(0x01, pwmvalue); motor_i2cWrite(0x02, 0); }
+            case 0:
+                if (richting) { 
+                    i2c_w(0x01, snelheid);
+		    i2c_w(0x02, 0);
+                }
                 else { 
-                    motor_i2cWrite(0x02, pwmvalue); motor_i2cWrite(0x01, 0); }
+		    i2c_w(0x01, 0);
+                    i2c_w(0x02, snelheid);
+                }
                 break;
-            case 2: // M2电机控制
-                if (direction) { 
-                    motor_i2cWrite(0x04, pwmvalue); motor_i2cWrite(0x03, 0); }
+            case 1:
+                if (richting) { 
+		    i2c_w(0x03, 0);
+                    i2c_w(0x04, snelheid);
+                }
                 else { 
-                    motor_i2cWrite(0x03, pwmvalue); motor_i2cWrite(0x04, 0); }
+                    i2c_w(0x03, snelheid);
+		    i2c_w(0x04, 0);
+                }
                 break;
         }
     }
 
 
-    //% block="LED Show"
-    //% group="RGB LED" weight=65
-    export function led_show() {
-        let a, s, d;
-
-        motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 255);
-        motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 255);
-        //红色逐渐点亮
-        for (a = 255; a > 1; a--) {
-            motor_i2cWrite(0x08, a);
-            motor_i2cWrite(0x09, a);
-            basic.pause(5);
-        }
-        //绿色逐渐点亮
-        for (s = 255; s > 1; s--) {
-            motor_i2cWrite(0x07, s);
-            motor_i2cWrite(0x0a, s);
-            basic.pause(5);
-        }
-        //红色逐渐熄灭
-        for (a = 0; a < 255; a++) {
-            motor_i2cWrite(0x08, a);
-            motor_i2cWrite(0x09, a);
-            basic.pause(5);
-        }
-        //blue
-        for (d = 255; d > 1; d--) {
-            motor_i2cWrite(0x06, d);
-            motor_i2cWrite(0x05, d);
-            basic.pause(5);
-        }
-        //green
-        for (s = 0; s < 255; s++) {
-            motor_i2cWrite(0x07, s);
-            motor_i2cWrite(0x0a, s);
-            basic.pause(5);
-        }
-        //rad
-        for (a = 255; a > 1; a--) {
-            motor_i2cWrite(0x08, a);
-            motor_i2cWrite(0x09, a);
-            basic.pause(5);
-        }
-        for (d = 0; d < 255; d++) {
-            motor_i2cWrite(0x06, d);
-            motor_i2cWrite(0x05, d);
-            basic.pause(5);
-        }
-        for (a = 0; a < 255; a++) {
-            motor_i2cWrite(0x08, a);
-            motor_i2cWrite(0x09, a);
-            basic.pause(5);
-        }
-
-
-    }
-
-    //% block="LED_R= |%color PWM= |$value"
+    //% block="led = |$nr rood = |$r groen = |$g blauw = |$b"
     //% direction.shadow=timePicker
-    //% value.min=0 value.max=255
-    //% group="RGB LED" weight=66
-    export function PWM_LED_R(color: pwm_led_r, value: number) {
-        motor_i2cWrite(color, value);
-    }
-    //% block="LED_L= |%color PWM= |$value"
-    //% direction.shadow=timePicker
-    //% value.min=0 value.max=255
-    //% group="RGB LED" weight=67
-    export function PWM_LED_L(color: pwm_led_l, value: number) {
-        motor_i2cWrite(color, value);
-    }
-
-    //% block="LED OFF"
-    //% group="RGB LED" weight=64
-    export function LED_OFF() {
-        motor_i2cWrite(0x08, 255); motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 255);
-        motor_i2cWrite(0x09, 255); motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 255);
-    }
-
-
-    //% block="RGB = |%place color = |$color"
-    //% direction.shadow=timePicker
-    //% group="RGB LED" weight=68
-    export function led_rgb(place: LED_rgb_L_R, color: LED_color) {
-        if (place == 1) {
-            switch (color) {
-                case 1: { motor_i2cWrite(0x08, 0); motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 255); };
-                    break;
-                case 2: { motor_i2cWrite(0x08, 255); motor_i2cWrite(0x07, 0); motor_i2cWrite(0x06, 255); };
-                    break;
-                case 3: { motor_i2cWrite(0x08, 255); motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 0); };
-                    break;
-                case 4: { motor_i2cWrite(0x08, 255); motor_i2cWrite(0x07, 0); motor_i2cWrite(0x06, 0); };
-                    break;
-                case 5: { motor_i2cWrite(0x08, 0); motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 0); };
-                    break;
-                case 6: { motor_i2cWrite(0x08, 0); motor_i2cWrite(0x07, 0); motor_i2cWrite(0x06, 0); };
-                    break;
-                case 7: { motor_i2cWrite(0x08, 0); motor_i2cWrite(0x07, 0); motor_i2cWrite(0x06, 255); };
-                    break;
-                case 8: { motor_i2cWrite(0x08, 255); motor_i2cWrite(0x07, 255); motor_i2cWrite(0x06, 255); };
-                    break;
-            }
-        }
-        if (place == 0) {
-            switch (color) {
-                case 1: { motor_i2cWrite(0x09, 0); motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 255); };
-                    break;
-                case 2: { motor_i2cWrite(0x09, 255); motor_i2cWrite(0x0a, 0); motor_i2cWrite(0x05, 255); };
-                    break;
-                case 3: { motor_i2cWrite(0x09, 255); motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 0); };
-                    break;
-                case 4: { motor_i2cWrite(0x09, 255); motor_i2cWrite(0x0a, 0); motor_i2cWrite(0x05, 0); };
-                    break;
-                case 5: { motor_i2cWrite(0x09, 0); motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 0); };
-                    break;
-                case 6: { motor_i2cWrite(0x09, 0); motor_i2cWrite(0x0a, 0); motor_i2cWrite(0x05, 0); };
-                    break;
-                case 7: { motor_i2cWrite(0x09, 0); motor_i2cWrite(0x0a, 0); motor_i2cWrite(0x05, 255); };
-                    break;
-                case 8: { motor_i2cWrite(0x09, 255); motor_i2cWrite(0x0a, 255); motor_i2cWrite(0x05, 255); };
-                    break;
-            }
-        }
-    }
+    //% r.min=0 r.max=255
+    //% g.min=0 g.max=255
+    //% b.min=0 b.max=255
+    //% group="LED" weight=68
+    export function my_led_rgb(nr: LED_rgb_L_R, r: number, g: number, b: number) {
+       if (nr == 0) {
+           i2c_w(8, r);
+           i2c_w(7, g);
+           i2c_w(6, b);
+       };
+       if (nr == 1) {
+           i2c_w(9, r);
+           i2c_w(10, g);
+           i2c_w(5, b);
+       };
 
     /**
     * Ultrasonic sensor
@@ -278,9 +140,9 @@ namespace MiniCar {
     }
 }
 
-function motor_i2cWrite(reg: number, value: number) {
+function i2c_w(reg: number, value: number) {
     let buf = pins.createBuffer(2)
     buf[0] = reg
     buf[1] = value
-    pins.i2cWriteBuffer(address, buf)
+    pins.i2cWriteBuffer(i2c_address, buf)
 }
