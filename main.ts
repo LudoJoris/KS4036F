17a153cf62742e3e1ba4f8cdb08768aa8373fe31
lujo <58606086+LudoJoris@users.noreply.github.com>
2025-04-05 13:33:40 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let i2c_address = 48
 
-enum Motorlist {
+enum Motor {
   //% block="links"
   M0 = 0,
   //% block="rechts"
@@ -24,78 +24,76 @@ enum LED_rgb_L_R {
 //% color="#AA278D"
 namespace SmartCar {
 
-    //% block="motor = | $motor richting = | $richting snelheid = $snelheid"
-    //% snelheid.min=0 snelheid.max=255 snelheid.defl=100
-    //% group="Motor" weight=65
-    export function motor(motor: Motorlist, richting: Richting, snelheid: number) {
-        switch (motor) {
-            case 0:
-                if (richting) { 
-                    i2c_w(0x01, snelheid);
-		    i2c_w(0x02, 0);
-                }
-                else { 
-		    i2c_w(0x01, 0);
-                    i2c_w(0x02, snelheid);
-                }
-                break;
-            case 1:
-                if (richting) { 
-		    i2c_w(0x03, 0);
-                    i2c_w(0x04, snelheid);
-                }
-                else { 
-                    i2c_w(0x03, snelheid);
-		    i2c_w(0x04, 0);
-                }
-                break;
-        }
+  //% block="motor = | $motor richting = | $richting snelheid = $snelheid"
+  //% snelheid.min=0 snelheid.max=255 snelheid.defl=100
+  //% group="Motor" weight=65
+  export function motor(motor: Motor, richting: Richting, snelheid: number) {
+    if (motor == 0) {
+      if (richting == 0) { 
+        i2c_w(0x01, 0);
+        i2c_w(0x02, snelheid);
+      }
+      if (richting == 1) { 
+        i2c_w(0x01, snelheid);
+        i2c_w(0x02, 0);
+      }
     }
+    if (motor == 1) {
+      if (richting == 0) { 
+        i2c_w(0x03, snelheid);
+        i2c_w(0x04, 0);
+      }
+      if (richting == 1) { 
+        i2c_w(0x03, 0);
+        i2c_w(0x04, snelheid);
+      }
+    }
+  }
 
-    //% block="motor = | $motor stop"
-    //% group="Motor" weight=66
-    export function stop(motor: Motorlist) {
-	if (motor == 0) {
-	    i2c_w(0x01, 0);
-	    i2c_w(0x02, 0);
-	}
-	if (motor == 1) {
-	    i2c_w(0x03, 0);
-	    i2c_w(0x04, 0);
-        }
+  //% block="motor = | $motor stop"
+  //% group="Motor" weight=66
+  export function stop(motor: Motor) {
+    if (motor == 0) {
+      i2c_w(0x01, 0);
+      i2c_w(0x02, 0);
     }
+    if (motor == 1) {
+      i2c_w(0x03, 0);
+      i2c_w(0x04, 0);
+    }
+  }
 
-    //% weight=10
-    //% block="rood $red|groen $green|blauw $blue"
-    //% red.min=0 red.max=255
-    //% green.min=0 green.max=255
-    //% blue.min=0 blue.max=255
-    export function rgb(red: number, green: number, blue: number): number {
-        return packRGB(red, green, blue);
-    }
+  //% weight=10
+  //% block="rood $red|groen $green|blauw $blue"
+  //% red.min=0 red.max=255
+  //% green.min=0 green.max=255
+  //% blue.min=0 blue.max=255
+  export function rgb(red: number, green: number, blue: number): number {
+    return packRGB(red, green, blue);
+  }
 
 }
 
 function packRGB(r: number, g: number, b: number): number {
-    return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
+  return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
 }
 function unpackR(rgb: number): number {
-    let r = (rgb >> 16) & 0xFF;
-    return r;
+  let r = (rgb >> 16) & 0xFF;
+  return r;
 }
 function unpackG(rgb: number): number {
-    let g = (rgb >> 8) & 0xFF;
-    return g;
+  let g = (rgb >> 8) & 0xFF;
+  return g;
 }
 function unpackB(rgb: number): number {
-    let b = (rgb) & 0xFF;
-    return b;
+  let b = (rgb) & 0xFF;
+  return b;
 }
 
 function i2c_w(reg: number, value: number) {
-    let buf = pins.createBuffer(2)
-    buf[0] = reg
-    buf[1] = value
-    pins.i2cWriteBuffer(i2c_address, buf)
+  let buf = pins.createBuffer(2)
+  buf[0] = reg
+  buf[1] = value
+  pins.i2cWriteBuffer(i2c_address, buf)
 }
 
